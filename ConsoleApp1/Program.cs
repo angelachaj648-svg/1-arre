4284b241679a786d257e2b0c7673dbb9ab415b83
angela chaj <angelachaj648@gmail.com>
2026-04-09 21:24:03 -0600

    solicite numero usando for

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Solicitar al usuario la cantidad n de números, guardarlos en un arreglo y mostrarlos en pan
+
+Console.Write("Cantidad: ");
+int n = int.Parse(Console.ReadLine());
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+	Console.Write("Número: ");
+	numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+	Console.Write(numeros[i] + " ");
+}
+
